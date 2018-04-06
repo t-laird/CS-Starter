32311c35feb06ed9f4cf0f503b42051c71226469
t-laird <t6r6l5@gmail.com>
2018-04-06 08:51:24 -0600

    reservation endpoints

--- src/main/java/com/campspot/dao/ReservationDAO.java
+++ src/main/java/com/campspot/dao/ReservationDAO.java
@@ -1,4 +1,20 @@
 package com.campspot.dao;
 
-public class ReservationDAO {
+
+import com.campspot.domain.Reservation;
+import com.campspot.mapper.ReservationMapper;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+
+import java.util.List;
+
+@RegisterMapper(ReservationMapper.class)
+public interface ReservationDAO {
+
+    @SqlQuery("select * from reservations;")
+    public List<Reservation> getReservations();
+
+    @SqlQuery("select * from reservations where id = :id")
+    public Reservation getReservation(@Bind("id") final int id);
 }
