a1ca3baaaf49c044678ee700234631af16d68947
t-laird <t6r6l5@gmail.com>
2018-04-06 08:50:00 -0600

    updated resource files

--- src/test/java/io/dropwizard/resources/HelloWorldResourceTest.java
+++ src/test/java/io/dropwizard/resources/HelloWorldResourceTest.java
@@ -1,4 +1,45 @@
 package io.dropwizard.resources;
 
+import com.campspot.api.Saying;
+import com.campspot.resources.HelloWorldResource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HelloWorldResourceTest {
+    private HelloWorldResource resource;
+
+    @Before
+    public void setup() {
+        resource = new HelloWorldResource("Hello, %s", "Stranger");
+    }
+
+    @Test
+    public void idStartsAtOne() {
+        Saying result = resource.sayHello(Optional.of("dropwizard"));
+        assertThat(result.getId()).isEqualTo(1);
+    }
+
+    @Test
+    public void idIncrementsByOne() {
+        Saying result = resource.sayHello(Optional.of("dropwizard"));
+        Saying result2 = resource.sayHello(Optional.of("dropwizard2"));
+
+        assertThat(result2.getId()).isEqualTo(result.getId() + 1);
+    }
+
+    @Test
+    public void absentNameReturnsDefaultName() {
+        Saying result = resource.sayHello(Optional.empty());
+        assertThat(result.getContent()).contains("Stranger");
+    }
+
+    @Test
+    public void nameReturnsName() {
+        Saying result = resource.sayHello(Optional.of("dropwizard"));
+        assertThat(result.getContent()).contains("dropwizard");
+    }
 }
